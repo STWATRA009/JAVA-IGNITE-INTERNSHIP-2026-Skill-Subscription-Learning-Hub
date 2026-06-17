20d274b644a2139bba7620f49bbcd033cf560284
ẞtwátrà Krûßhñâ Máñgâràj <skmangaraj2006@gmail.com>
2026-06-18 00:22:11 +0530

    Update SkillSubscriptionLearningHub2026Application.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/example/demo/SkillSubscriptionLearningHub2026Application.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/example/demo/SkillSubscriptionLearningHub2026Application.java
@@ -8,6 +8,7 @@ public class SkillSubscriptionLearningHub2026Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkillSubscriptionLearningHub2026Application.class, args);
+		System.out.println("Springboot has been successfully launched!");
 	}
 
 }
